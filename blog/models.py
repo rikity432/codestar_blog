fbbe4eaa330780e1337dd92878bb59e33b9e4b54
rikity432 <andre.shaw.wow@gmail.com>
2026-01-29 15:11:41 +0000

    Register Post in admin

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,16 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-User = get_user_model()
-
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+)
 
 class Post(models.Model):
-    STATUS = (
-        (0, "Draft"),
-        (1, "Published"),
-    )
-
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="blog_posts",
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
