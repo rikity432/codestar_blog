e60e244464e537ca35c0d690142d5bc423dde74c
rikity432 <andre.shaw.wow@gmail.com>
2026-02-04 21:54:12 +0000

    create about app

---
+++ about/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from .models import About
+
+
+def about_me(request):
+	"""Render the About page."""
+	about = About.objects.all().order_by("-updated_on").first()
+
+	return render(
+		request,
+		"about/about.html",
+		{"about": about},
+	)
